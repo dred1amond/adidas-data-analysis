55b8cde902a427c476fa737109a26fce97e72927
dred1amond <demodoston@gmail.com>
2025-12-31 15:48:55 +0500

    Add SQL query for core KPIs calculation

--- sql/02_core_kpis.sql
+++ sql/02_core_kpis.sql
@@ -1,1 +1,6 @@
-
+SELECT
+    SUM(total_sales) AS total_revenue,
+    SUM(operating_profit) AS total_operating_profit,
+    ROUND(AVG(operating_margin), 2) AS avg_operating_margin,
+    SUM(units_sold) AS total_units_sold
+FROM sales_clean;
