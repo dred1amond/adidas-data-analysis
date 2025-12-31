836d43789a347167533a2ee6228baabe37e94cdd
dred1amond <demodoston@gmail.com>
2025-12-31 15:50:53 +0500

    Add sales method analysis query

--- sql/05_sales_method_analysis.sql
+++ sql/05_sales_method_analysis.sql
@@ -1,1 +1,9 @@
-
+SELECT
+    sales_method,
+    COUNT(*) AS transactions,
+    SUM(total_sales) AS revenue,
+    SUM(operating_profit) AS profit,
+    ROUND(AVG(operating_margin), 2) AS avg_margin
+FROM sales_clean
+GROUP BY sales_method
+ORDER BY avg_margin DESC;
