548fc2219ad8366145d65aae9789750a79335b2f
dred1amond <demodoston@gmail.com>
2025-12-31 15:49:39 +0500

    Add profitability analysis query by region

--- sql/03_profitability_analysis.sql
+++ sql/03_profitability_analysis.sql
@@ -1,1 +1,8 @@
-
+SELECT
+    region,
+    SUM(total_sales) AS revenue,
+    SUM(operating_profit) AS profit,
+    ROUND(SUM(operating_profit) / NULLIF(SUM(total_sales), 0) * 100, 2) AS profit_margin
+FROM sales_clean
+GROUP BY region
+ORDER BY profit_margin DESC;
